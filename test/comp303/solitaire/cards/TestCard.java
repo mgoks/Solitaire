b458181f672dbeab816106b68947fc9e4a89d7dd
Halil-Murat <31682549+Halil-Murat@users.noreply.github.com>
2017-10-23 01:27:31 -0400

    added testSameColorAs() and testToString()

--- test/comp303/solitaire/cards/TestCard.java
+++ test/comp303/solitaire/cards/TestCard.java
@@ -5,18 +5,28 @@ import comp303.solitaire.cards.Card;
 import comp303.solitaire.cards.Card.Rank;
 import comp303.solitaire.cards.Card.Suit;
 import static org.junit.Assert.*;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Modifier;
 
 public class TestCard
 {
 	@Test
-	public void testInit() throws Exception
+	public void testSameColorAs() throws Exception
 	{
-		Constructor<Card> constructor = Card.class.getDeclaredConstructor();
-		assertTrue(Modifier.isPrivate(constructor.getModifiers()));
-		constructor.setAccessible(true);
-		Card c = constructor.newInstance(Rank.ACE, Suit.SPADES);
-		
+		Card aceOfSpades   = Card.get(Rank.ACE, Suit.SPADES);
+		Card aceOfClubs    = Card.get(Rank.ACE, Suit.CLUBS);
+		Card aceOfHearts   = Card.get(Rank.ACE, Suit.HEARTS);
+		Card aceOfDiamonds = Card.get(Rank.ACE, Suit.DIAMONDS);
+		assertEquals(true, aceOfDiamonds.sameColorAs(aceOfHearts));
+		assertEquals(true, aceOfHearts.sameColorAs(aceOfDiamonds));
+		assertEquals(true, aceOfSpades.sameColorAs(aceOfClubs));
+		assertEquals(true, aceOfClubs.sameColorAs(aceOfSpades));
+		assertEquals(false, aceOfClubs.sameColorAs(aceOfHearts));
+		assertEquals(true, aceOfSpades.sameColorAs(aceOfSpades));
+	}
+	
+	@Test
+	public void testToString() throws Exception
+	{
+		Card c = Card.get(Rank.ACE, Suit.SPADES);
+		assertEquals("ACE of SPADES", c.toString());
 	}
 }
